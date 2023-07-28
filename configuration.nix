b7b717e963c02c426635e20a7c8652cb8b07316b
Brooks J Rady <b.j.rady@gmail.com>
2023-07-29 00:03:26 +0100

    Start daily driving

--- configuration.nix
+++ configuration.nix
@@ -6,66 +6,98 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ # Include the results of the hardware scan
       ./hardware-configuration.nix
+      # Include Home Manager
+      <home-manager/nixos>
     ];
 
-  # Use the systemd-boot EFI boot loader.
+  # Enable Nix flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Use the latest Linux kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  # Define your hostname
+  networking.hostName = "synchrotron";
 
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  # Enable networking
+  networking.networkmanager.enable = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Set your time zone
+  time.timeZone = "Europe/London";
 
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkbOptions in tty.
-  # };
+  # Select internationalisation properties
+  i18n.defaultLocale = "en_GB.UTF-8";
 
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
   };
 
+  # Enable the X11 windowing system
+  services.xserver.enable = true;
+  services.xserver.excludePackages = [ pkgs.xterm ];
+
+  # Enable the GNOME Desktop Environment
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Clean up the default GNOME install a bit...
+  environment.gnome.excludePackages = with pkgs.gnome; [
+    cheese      # photo booth
+    eog         # image viewer
+    gedit       # text editor
+    simple-scan # document scanner
+    totem       # video player
+    yelp        # help viewer
+    geary       # email client
+    seahorse    # password manager
+
+    # these should be self explanatory
+    gnome-contacts gnome-font-viewer gnome-logs gnome-maps gnome-music
+    pkgs.gnome-photos gnome-screenshot pkgs.gnome-connections pkgs.gnome-tour
+  ];
 
   # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = {
-  #   "eurosign:e";
-  #   "caps:escape" # map caps to escape.
-  # };
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  # Absolutely do not ask for any passwords graphically...
+  programs.ssh.askPassword = "";
 
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+  # Enable CUPS to print documents
+  services.printing.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  # Enable sound with pipewire
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.tll = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    description = "Brooks J Rady";
+    extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       firefox
       mailspring
@@ -74,17 +106,30 @@
       logseq
       gimp
       bitwarden
-      # bespokesynth
       # cq-editor
       discord
       furtherance
     ];
   };
 
+  # Can this be merged with the above?
+  home-manager.users.tll = { pkgs, ... }: {
+    home.stateVersion = "23.05";
+    programs.git = {
+      enable = true;
+      userName = "Brooks J Rady";
+      userEmail = "b.j.rady@gmail.com";
+    };
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    neovim
+    helix
+    zellij
+    fd
+    ripgrep
+    bat
   ];
 
   # Whitelist a number of nonfree applications
@@ -93,32 +138,13 @@
     "discord"
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # Enable the OpenSSH daemon
+  services.openssh.enable = true;
 
   # Enable automatic package upgrades
   system.autoUpgrade.enable = true;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -126,6 +152,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
+  system.stateVersion = "23.05"; # Did you read the comment?
 
 }
